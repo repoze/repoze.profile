0e22e3b2521e7f9af8a2539e4ad99d88213a308e
Chris McDonough <chrism@plope.com>
2007-10-20 02:22:18 +0000

    A directory representing a template that we can use for new repoze.* namespace projects.

---
+++ setup.py
@@ -0,0 +1,58 @@
+##############################################################################
+#
+# Copyright (c) 2007 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+__version__ = '0.1'
+
+import os
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+setup(name='repoze.atemplate',
+      version=__version__,
+      description='A template for repoze projects',
+      long_description=README,
+      classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      keywords='web application server wsgi zope',
+      author="Agendaless Consulting",
+      author_email="repoze-dev@lists.repoze.org",
+      dependency_links=['http://dist.repoze.org'],
+      url="http://www.repoze.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['repoze'],
+      zip_safe=False,
+      tests_require = [],
+      install_requires=[],
+      #test_suite="repoze.",
+      cmdclass = {'sandbox': sandbox},
+      entry_points = {
+        'console_scripts': [],
+      },
+      )
+
